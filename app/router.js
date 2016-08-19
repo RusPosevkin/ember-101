3dd1b630e6cb24d18c97c30a6feae6efe6f5b74f
posevkin <posevkin@loyaltyplant.com>
2016-08-19 19:24:34 +0300

    init

---
+++ app/router.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('friends', function() {
+    this.route('new');
+
+    this.route('show', {
+      path: ':friend_id'
+    }, function() {
+      this.route('loans', {resetNamespace: true}, function() {
+        this.route('new');
+      });
+    });
+
+    this.route('edit', {
+      path: ':friend_id/edit'
+    });
+  });
+
+  this.route('articles', function() {
+    this.route('new');
+
+    this.route('show', {
+      path: ':article_id'
+    });
+
+    this.route('edit', {
+      path: ':article_id/edit'
+    });
+  });
+  // this.route('loans');
+});
+
+export default Router;
+
+
---
+++ ember-cli-build.js
@@ -0,0 +1,25 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+  });
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.eot', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.svg', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/fontello.woff2', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/basscss.min.css');
+
+  return app.toTree();
+};
